f6a70bca9a9da15007fa093c4d0e70383300ad43
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2024-08-22 11:48:43 +0200

    add nightingale structure

---
+++ nightingale-structure/dist/index.d.ts
@@ -0,0 +1,5 @@
+import NightingaleStructure from "./nightingale-structure";
+export type * from "./nightingale-structure";
+export type * from "./position-mapping";
+export default NightingaleStructure;
+//# sourceMappingURL=index.d.ts.map
