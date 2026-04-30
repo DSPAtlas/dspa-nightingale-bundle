3886ae6b81fadefe8ed245614b43196a1ad525e0
audrius <audrius.meskauskas@ethz.ch>
2026-04-30 14:16:07 +0200

    Changed LIP color scale.

--- nightingale-structure/src/color_new.ts
+++ nightingale-structure/src/color_new.ts
@@ -94,11 +94,11 @@ const getInterpolatedLipColor = (score: number) => {
 // LIP Color Scale. This is exportable, to reuse as LIP_COLOR_SCALE in NightingaleComponent.jsx
 // Normalized [0, 1] range (scores from backend are min-max normalized)
 export const LIP_SCALE = [
-    { threshold: 1.0,        color: '#289b22', label: 'Very High ( ≥ 0.8 )' },
-    { threshold: 0.8,       color: '#da49a9', label: 'High ( 0.7 - 0.8 )' },
-    { threshold: 0.6,       color: '#f2c0e1', label: 'Medium-High ( 0.6 - 0.7 )' },
-    { threshold: 0.4,       color: '#fbeaf5', label: 'Medium ( 0.4-0.6 )' },
-    { threshold: 0.0,        color: '#acc1db', label: 'Low ( < 0.4 )' },
+    { threshold: 1.0,        color: '#da49a9', label: 'Very High ( ≥ 0.8 )' },
+    { threshold: 0.8,        color: '#f2c0e1', label: 'High ( 0.7 - 0.8 )' },
+    { threshold: 0.6,        color: '#fbeaf5', label: 'Medium-High ( 0.6 - 0.7 )' },
+    { threshold: 0.4,        color: '#acc1db', label: 'Medium ( 0.4-0.6 )' },
+    { threshold: 0.0,        color: '#346aae', label: 'Low ( < 0.4 )' },
     { threshold: -Infinity,  color: '#3f3d3d', label: 'No coverage' },
 ];
 
